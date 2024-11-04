8910f77e118595e96e80226e8c67281b606c5e5e
Kjett0701 <kjett9@ivytech.edu>
2024-11-04 00:12:14 +0000

    Testing Mon Nov 04 2024 00:12:14 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex01/student/equilateral.py
+++ chapter3/ex01/student/equilateral.py
@@ -1,8 +1,19 @@
 # Write your program here 
 
-input("Enter the first side: ") 
-input("Enter the second side: ") 
-input("Enter the third side: ") 
+def sides(side1 , side2 , side3):
+    return side1 == side2 == side3
+
+
+side1 = input("Enter the first side: ") 
+side2 = input("Enter the second side: ") 
+side3 = input("Enter the third side: ") 
+
+if sides:
+    print("The triangle is equilateral") 
+else:
+    print("The triangle is NOT equilateral")
+
+
 
 
 
